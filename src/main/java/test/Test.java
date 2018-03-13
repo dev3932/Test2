efcf5a523c24308e74fa22c7c2cff6d45bc93c2a
lg <2573703932@qq.com>
2018-03-13 13:51:36 +0800

    --

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,6 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("github使用测试");
+        System.out.println("github使用测试ss");
     }
 }
